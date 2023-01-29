0ffa0294b10213fa6d8eaa8f364f9860b4c5a9f4
dengx <55045227+dengx6@users.noreply.github.com>
2023-01-29 09:24:10 +0800

    Update gstream.cpp

--- gstream/gstream.cpp
+++ gstream/gstream.cpp
@@ -2,13 +2,11 @@
 
 
 gstream::gstream(string _data_path){
-//	cout << "init datastream" << endl;
 	fin.open(_data_path.c_str(), ios::in);
 	if(!fin){
 		cerr << "can not open " << _data_path << endl;
 		system("pause");
 	}
-//	cout << "init finish datastream" << endl;
 }
 
 gstream::~gstream(){
@@ -32,18 +30,6 @@ Edge* gstream::next(){
 	fin.getline(_buf, 4999, '\n');
 	Edge* _newedge = new Edge(_buf);
 	return _newedge;
-	
-/*
-	if(this->cur_itr != this->alledges.end())
-	{
-		dEdge* _ret = *(this->cur_itr);
-		++ this->cur_itr;
-		return _ret;
-	}
-	return NULL;
-	**/
 }
-
-//private:
 
 
