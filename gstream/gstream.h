c3eb915c3cd5ac21419ee5e9ddd924b32f833f56
dengx <55045227+dengx6@users.noreply.github.com>
2023-01-29 09:26:43 +0800

    Update gstream.h

--- gstream/gstream.h
+++ gstream/gstream.h
@@ -6,22 +6,10 @@ class gstream{
 public:
 	gstream(string _data_path);
 	~gstream();
-	/*
-	 * check empty of alledges
-	 * read all edges into alledges
-	 * call reset();
-	 */
-	//virtual bool load_edges(int _avg_win_tuple_num) = 0;
-	//virtual bool is_expire(dEdge* _e_old, dEdge* _e_new) = 0;
-	//bool reset();
 	bool hasnext();
 	Edge * next();
-	int size();
 
-protected:
-	//vector<Edge*>::iterator cur_itr;//迭代器
-	//string data_path;
-	//vector<Edge*> snapshot;
+
 private:
 	ifstream fin;
 
